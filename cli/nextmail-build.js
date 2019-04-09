05681f162c0802361886c22df58d25a5e32ee968
Sean Connolly <sconnolly17@gmail.com>
2019-04-09 00:11:04 -0600

    Do all the files

--- cli/nextmail-build.js
+++ cli/nextmail-build.js
@@ -1,13 +1,19 @@
 const path = require('path');
 const fs = require('fs').promises;
+var requireContext = require('require-context');
 const { transformFileAsync } = require('@babel/core');
 const babelPreset = require('../lib/babel-preset');
 
 async function nextmailBuild() {
   const root = process.cwd();
-  fs.mkdir(`${root}/.nextmail/emails`, { recursive: true });
-  const result = await transformFileAsync(`${root}/emails/demo.js`, babelPreset);
-  await fs.writeFile(`${root}/.nextmail/emails/demo.js`, result.code);
+  fs.mkdir(`${root}/.nextmail`, { recursive: true });
+
+  const req = requireContext(`${root}/emails`, true, /\.js$/);
+
+  req.keys().forEach(async (filename) => {
+    const result = await transformFileAsync(`${root}/emails/${filename}`, babelPreset);
+    await fs.writeFile(`${root}/.nextmail/${filename}`, result.code);
+  });
 };
 
 module.exports = nextmailBuild;
