4311edaccd977189d873fb88aa2a54e49db4ef92
Sean Connolly <sconnolly17@gmail.com>
2019-04-09 00:25:34 -0600

    Render the email dynamically

--- examples/cli/emails/demo.js
+++ examples/cli/emails/demo.js
@@ -1,5 +1,30 @@
+import React from 'react';
+
+import {
+  Mjml,
+  MjmlHead,
+  MjmlTitle,
+  MjmlBody,
+  MjmlSection,
+  MjmlColumn,
+  MjmlText
+} from 'mjml-react';
+
 function Demo() {
-  return <div>Welcome to Nextmail!</div>;
+  return (
+    <Mjml>
+      <MjmlHead>
+        <MjmlTitle>Last Minute Offer</MjmlTitle>
+      </MjmlHead>
+      <MjmlBody width={500}>
+        <MjmlSection fullWidth backgroundColor="#efefef">
+          <MjmlColumn>
+            <MjmlText align="center">Such a small template.</MjmlText>
+          </MjmlColumn>
+        </MjmlSection>
+      </MjmlBody>
+    </Mjml>
+  );
 }
 
 export default Demo;
--- examples/cli/index.js
+++ examples/cli/index.js
@@ -1,3 +1,3 @@
 const { renderEmail } = require('nextmail');
 
-console.log('renderEmail', renderEmail('abc'));
+console.log('renderEmail', renderEmail('demo'));
