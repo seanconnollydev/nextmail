3802cf9bdc714715be248fe5c2ed0f23f364655f
Sean Connolly <sconnolly17@gmail.com>
2019-04-15 01:13:46 -0600

    Handle the unhandled

--- server/startServer.js
+++ server/startServer.js
@@ -3,6 +3,9 @@ const http = require('http');
 const handleRequest = require('./handleRequest');
 
 const startServer = async () => {
+  process.on('unhandledRejection', (reason) => {
+    console.log('Unhandled Rejection at:', reason.stack || reason);
+  });
   const server = http.createServer(handleRequest);
 
   await new Promise((resolve, reject) => {
@@ -10,6 +13,7 @@ const startServer = async () => {
       if (err) {
         reject(err);
       }
+
       console.log('server is listening on 6100');
       resolve();
     });
