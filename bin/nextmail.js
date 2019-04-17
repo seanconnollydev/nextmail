f68e31f70b6ab27cc86f0eb712b10a90fc423e94
Sean Connolly <sconnolly17@gmail.com>
2019-04-16 22:20:20 -0600

    Error checking

--- bin/nextmail.js
+++ bin/nextmail.js
@@ -2,16 +2,20 @@
 const { argv } = require('yargs');
 
 (async () => {
-  const command = argv._[0];
-  const forwardedArgs = argv._.slice(1);
+  try {
+    const command = argv._[0];
+    const forwardedArgs = argv._.slice(1);
 
-  if (command === 'build') {
-    await require('../cli/nextmail-build')(forwardedArgs);
-  } else if (command === 'start') {
-    await require('../cli/nextmail-start')(forwardedArgs);
-  } else if (command === 'send') {
-    await require('../cli/nextmail-send')(forwardedArgs);
-  } else {
-    await require('../cli/nextmail-dev')(forwardedArgs);
+    if (command === 'build') {
+      await require('../cli/nextmail-build')(forwardedArgs);
+    } else if (command === 'start') {
+      await require('../cli/nextmail-start')(forwardedArgs);
+    } else if (command === 'send') {
+      await require('../cli/nextmail-send')(forwardedArgs);
+    } else {
+      await require('../cli/nextmail-dev')(forwardedArgs);
+    }
+  } catch (err) {
+    console.error(err); // eslint-disable-line no-console
   }
 })();
